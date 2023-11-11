b1d221e564457c82fc6421f95e1c01e28ba88a2a
Samarpit Shrivastava <42511743+samarpit30@users.noreply.github.com>
2023-11-11 09:44:30 +0000

    Organizing Files - First draft

--- src/docs/5.35.x/navigation.js
+++ src/docs/5.35.x/navigation.js
@@ -50,6 +50,7 @@ export const Navigation = () => {
                     </Section>
                     <Section title={"File Manager"}>
                         <Page link={"user-guides/page-builder/file-manager/upload-file"} />
+                        <Page link={"user-guides/page-builder/file-manager/organizing-files"} />
                     </Section>
                 </Collapsable>
             </NavGroup>
